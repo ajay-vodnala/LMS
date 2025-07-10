37827eb5aac9388e072a645a5004eb32bdde3b4a
ajay-vodnala <ajayvodnala5496@gmail.com>
2025-07-10 18:02:23 +0530

    Update index.mjs

--- backendwork/index.mjs
+++ backendwork/index.mjs
@@ -27,6 +27,7 @@ const initalizeDbServer=async()=>{
         app.listen(port, () => {
         console.log(`Server started on port ${port}`);
    });
+    }
     catch(e){
         console.log(`error:${e.message}`);
         process.exit(1);
