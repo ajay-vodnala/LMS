7adb8a13a41b87b10b415a0830fd7e60467b6445
ajay-vodnala <ajayvodnala5496@gmail.com>
2025-07-11 16:41:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export const App =()=>{
      
       return(
         loading?<Preloader/>:
-          <Router>
+          <Router basename="/LMS">
               <Routes>
                 <Route exact path="/dashboard" element={<Interface/>} />
                 <Route path="/login" element={<Login />} />
