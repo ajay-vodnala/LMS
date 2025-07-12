b9c4a42484c0f041c89419fb1a1070f2bae4eacf
ajay-vodnala <ajayvodnala5496@gmail.com>
2025-07-12 16:22:26 +0530

    Update index.js

--- src/components/displayBooks/index.js
+++ src/components/displayBooks/index.js
@@ -4,13 +4,14 @@ const serverURL = process.env.REACT_APP_SERVER_URL;
 const DisplayBooks=(props)=>{
     const {bookDetails}=props;
     const{bookId,imageUrl,title,author,department,yearOfPublish,language,location,status}=bookDetails;
+    const imageSource=(imageUrl.includes('cloudinary'))?"imageUrl":`${serverURL}/uploads/books/${imageUrl}`;
     let statusColor=null;
     if(status==="available"){
         statusColor="greenText";
     }
     return(
         <tr className='dataRow'>
-            <td className='imageDiv'><img src={`${serverURL}/uploads/books/${imageUrl}`} className='bookImg' alt='Book Image'></img></td>
+            <td className='imageDiv'><img src={imageSourc} className='bookImg' alt='Book Image'></img></td>
             <td>{title}</td>
             <td>{author}</td>
             <td>{department}</td>
